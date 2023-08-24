36d761db9e00b998da15bb49d5678cc23aa04b1e
Seth Addo <addoseth82@gmail.com>
2023-08-24 12:14:01 +0000

    added comments

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,85 +1,96 @@
-import 'dart:developer';
-import 'package:camera/camera.dart';
-import 'package:flutter_tflite/flutter_tflite.dart';
-import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'dart:developer'; // Import the developer logging library
+import 'package:camera/camera.dart'; // Import the camera package
+import 'package:flutter_tflite/flutter_tflite.dart'; // Import the TensorFlow Lite package
+import 'package:get/get.dart'; // Import the Get package for state management
+import 'package:permission_handler/permission_handler.dart'; // Import the permission handling package
 
+// Define a GetxController class named ScanController
 class ScanController extends GetxController {
   @override
   void onInit() {
-    initCamera();
-    initTfLite();
+    initCamera(); // Initialize the camera
+    initTfLite(); // Initialize TensorFlow Lite model
     super.onInit();
   }
 
   @override
   void dispose() {
-    cameraController.dispose();
-    Tflite.close();
+    cameraController.dispose(); // Dispose of the camera controller
+    Tflite.close(); // Close TensorFlow Lite
     super.dispose();
   }
 
-  late CameraController cameraController;
-  late List<CameraDescription> cameras;
+  late CameraController cameraController; // Controller for camera
+  late List<CameraDescription> cameras; // List of available cameras
 
-  var isCameraInitialized = false.obs;
-  var cameraCount = 0;
-  var label = "";
+  var isCameraInitialized = false.obs; // Observable for camera initialization status
+  var cameraCount = 0; // Counter for camera frames processed
+  var label = ""; // Detected object label
 
+  // Function to initialize the camera
   initCamera() async {
+    // Request camera permission
     if (await Permission.camera.request().isGranted) {
-      cameras = await availableCameras();
-      cameraController = CameraController(cameras[0], ResolutionPreset.high,
-          imageFormatGroup: ImageFormatGroup.yuv420);
+      cameras = await availableCameras(); // Get available cameras
+      cameraController = CameraController(
+        cameras[0],
+        ResolutionPreset.high,
+        imageFormatGroup: ImageFormatGroup.yuv420,
+      );
       await cameraController.initialize().then((value) {
+        // Start processing camera frames
         cameraController.startImageStream((image) {
           cameraCount++;
           if (cameraCount % 50 == 0) {
             cameraCount = 0;
-            objectDetector(image);
+            objectDetector(image); // Run object detection on the frame
           }
-          update();
+          update(); // Update UI
         });
       });
-      isCameraInitialized(true);
-      update();
+      isCameraInitialized(true); // Update camera initialization status
+      update(); // Update UI
     } else {
-      print("Permission Denied");
+      print("Permission Denied"); // Log permission denial
     }
   }
 
+  // Function to initialize TensorFlow Lite model
   initTfLite() async {
     await Tflite.loadModel(
-        model: "assets/model_unquant.tflite",
-        labels: "assets/labels.txt",
-        isAsset: true,
-        numThreads: 1,
-        useGpuDelegate: false);
+      model: "assets/model_unquant.tflite", // Model file path
+      labels: "assets/labels.txt", // Labels file path
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
   }
 
+  // Function to perform object detection on a camera image
   objectDetector(CameraImage image) async {
     var detector = await Tflite.runModelOnFrame(
-        bytesList: image.planes.map((plane) {
-          return plane.bytes;
-        }).toList(),
-        imageHeight: image.height,
-        imageWidth: image.width,
-        imageMean: 127.5,
-        imageStd: 127.5,
-        rotation: 90,
-        numResults: 2,
-        threshold: 0.1,
-        asynch: true);
+      bytesList: image.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      rotation: 90,
+      numResults: 2,
+      threshold: 0.1,
+      asynch: true,
+    );
 
     if (detector != null) {
-      log("Result is $detector");
+      log("Result is $detector"); // Log the detection result
       var detectedObject = detector.first;
       if (detectedObject['confidence'] * 100 > 99) {
-        label = detectedObject['label'].toString();
+        label = detectedObject['label'].toString(); // Set the detected label
       } else {
-        label = "Scan a building";
+        label = "Scan a building"; // Set label if confidence is low
       }
-      update();
+      update(); // Update UI
     }
   }
 }
--- lib/views/camera_view.dart
+++ lib/views/camera_view.dart
@@ -1,17 +1,21 @@
+// Import necessary packages
 import 'package:building_recognition/controller/scan_controller.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
+// Define a widget class named CameraView
 class CameraView extends StatelessWidget {
+  // Constructor for the CameraView widget
   const CameraView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Build the UI using the Scaffold widget
     return Scaffold(
+      // Extend body behind the app bar
       extendBodyBehindAppBar: true,
+      // Customize the app bar
       appBar: AppBar(
         title: const Text(
           "UCC Buildings",
@@ -23,44 +27,59 @@ class CameraView extends StatelessWidget {
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(75)),
         ),
       ),
+      // Build the body of the Scaffold using GetBuilder widget from the Get package
       body: GetBuilder<ScanController>(
+        // Initialize the controller for the GetBuilder widget
         init: ScanController(),
+        // Build the UI based on the controller's state
         builder: (controller) {
+          // Check if the camera is initialized
           return controller.isCameraInitialized.value
               ? Column(
                   children: [
+                    // Create a camera preview area within a Stack
                     Expanded(
                       child: Stack(
                         children: [
+                          // Display the camera preview
                           SizedBox(
                             width: MediaQuery.of(context).size.width,
                             height: MediaQuery.of(context).size.height,
                             child: CameraPreview(controller.cameraController),
                           ),
+                          // Display a message to get closer to a building for accurate results
                           const Center(
-                              child: Text(
-                                  "kindly get closer to a building for accurate results.", style: TextStyle(
-                                    color: Colors.yellowAccent,
-                                    fontSize: 11
-                                  ),))
+                            child: Text(
+                              "kindly get closer to a building for accurate results.",
+                              style: TextStyle(
+                                color: Colors.yellowAccent,
+                                fontSize: 11,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
+                    // Display a container with building label
                     Container(
-                        height: 100,
-                        decoration: const BoxDecoration(
-                          color: Color.fromARGB(255, 33, 41, 54),
-                        ),
-                        child: Center(
-                            child: Text(
+                      height: 100,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 33, 41, 54),
+                      ),
+                      child: Center(
+                        child: Text(
                           controller.label,
                           style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold),
-                        )))
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 )
+              // Display a loading message if the camera is not initialized
               : const Center(
                   child: Text("Loading Camera..."),
                 );
